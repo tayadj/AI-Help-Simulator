792d49907ae107317bc8ea93f7c52bc00bbe7e91
tayadj <tayadj@users.noreply.github.com>
2025-03-26 22:00:22 +0300

    Database service implemented

--- src/database/src/main.py
+++ src/database/src/main.py
@@ -1,19 +1,33 @@
 import config
 import core
+import protocol
 
 import asyncio
+import concurrent
+import grpc
+import sqlalchemy.ext.asyncio
 
 
 
-if __name__ == '__main__':
+class DatabaseService(protocol.models_pb2_grpc.DatabaseServiceServicer):
 
-	async def main():
+    def __init__(self, database):
 
-		settings = config.Settings()
-		database = core.Database(settings.DATABASE_URL.get_secret_value())
+        self.database = database
 
-		async with database.session_local() as session:
+    async def CreateUser(self, request, context):
 
-				user = await database.model_user.create_user(session, 1, 'Name', 'email@domain.com', 'admin')
+        async with self.database.session_local() as session:
 
-	asyncio.run(main())
+            user = await self.database.model_user.create_user(
+                database = session, id = request.id, name = request.name, email = request.email, password = request.password
+            )
+            return UserResponse(id = user.id, name = user.name, email = user.email, password = user.password)
+
+def serve(database):
+
+    server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=10))
+    protocol.models_pb2_grpc.add_DatabaseServiceServicer_to_server(ModelsService(database), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
