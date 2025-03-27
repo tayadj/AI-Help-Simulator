fbc320f05dcdb2916b73603c07530adcc44337e8
tayadj <tayadj@users.noreply.github.com>
2025-03-27 10:03:31 +0300

    Beautify database protocol usage

--- src/application/src/main.py
+++ src/application/src/main.py
@@ -3,48 +3,24 @@ import os
 import grpc
 import sys
 
-sys.path.append(os.path.dirname(__file__) + '/../../database/src')
+sys.path.append(os.path.dirname(__file__) + '/../..')
 
-import protocol
+import database.src.protocol
 
 
 async def main():
 
     async with grpc.aio.insecure_channel("localhost:50051") as channel: 
     
-        stub = protocol.database_pb2_grpc.DatabaseServiceStub(channel)
+        stub = database.src.protocol.database_pb2_grpc.DatabaseServiceStub(channel)
 
-        request = protocol.database_pb2.UserRequest(
+        request = database.src.protocol.database_pb2.UserRequest(
             id = 1
         )
 
         response = await stub.ReadUser(request)  
         print(response)
 
-        request = protocol.database_pb2.ConfigRequest(
-            id = 1,
-            key = 'key',
-            value = 'value'
-        )
-
-        response = await stub.CreateConfig(request)
-        print(response)
-
-        request = protocol.database_pb2.UserRequest(
-            id = 2
-        )
-
-        response = await stub.DeleteUser(request)  
-        print(response)
-
-        request = protocol.database_pb2.ConfigRequest(
-            id = 1,
-            key = 'key',
-            value = 'new_value'
-        )
-
-        response = await stub.UpdateConfig(request)  
-        print(response)
 
 
 if __name__ == "__main__":
