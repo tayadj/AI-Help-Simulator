12d0ccd3c6a0a953dacc2b22c5276413a0ab96c4
tayadj <tayadj@users.noreply.github.com>
2025-03-27 09:14:04 +0300

    Implemented config model CRUD

--- src/application/src/main.py
+++ src/application/src/main.py
@@ -8,19 +8,45 @@ sys.path.append(os.path.dirname(__file__) + '/../../database/src')
 import protocol
 
 
-async def test_create_user():
+async def main():
 
     async with grpc.aio.insecure_channel("localhost:50051") as channel: 
     
         stub = protocol.database_pb2_grpc.DatabaseServiceStub(channel)
 
         request = protocol.database_pb2.UserRequest(
-            id=1
+            id = 1
         )
+
         response = await stub.ReadUser(request)  
-        print(f"{response}")
+        print(response)
+
+        request = protocol.database_pb2.ConfigRequest(
+            id = 1,
+            key = 'key',
+            value = 'value'
+        )
+
+        response = await stub.CreateConfig(request)
+        print(response)
+
+        request = protocol.database_pb2.UserRequest(
+            id = 2
+        )
+
+        response = await stub.DeleteUser(request)  
+        print(response)
+
+        request = protocol.database_pb2.ConfigRequest(
+            id = 1,
+            key = 'key',
+            value = 'new_value'
+        )
+
+        response = await stub.UpdateConfig(request)  
+        print(response)
 
 
 if __name__ == "__main__":
 
-    asyncio.run(test_create_user())
+    asyncio.run(main())
--- src/database/src/main.py
+++ src/database/src/main.py
@@ -48,17 +48,51 @@ class DatabaseService(protocol.database_pb2_grpc.DatabaseServiceServicer):
 
 			return protocol.database_pb2.UserResponse(id = user.id, name = user.name, email = user.email, password = user.password)
 
+	async def CreateConfig(self, request, context):
+
+		async with self.database.session_local() as session:
+
+			config = await self.database.model_config.create_config(database = session, id = request.id, key = request.key, value = request.value)
+
+			return protocol.database_pb2.ConfigResponse(id = request.id, key = request.key, value = request.value)
+
+	async def ReadConfig(self, request, context):
+
+		async with self.database.session_local() as session:
+
+			config = await self.database.model_config.read_config(database = session, id = request.id)
+
+			return protocol.database_pb2.ConfigResponse(id = request.id, key = request.key, value = request.value)
+
+	async def UpdateConfig(self, request, context):
+
+		async with self.database.session_local() as session:
+
+			config = await self.database.model_config.update_config(database = session, id = request.id, key = request.key, value = request.value)
+
+			return protocol.database_pb2.ConfigResponse(id = request.id, key = request.key, value = request.value)
+
+	async def DeleteConfig(self, request, context):
+
+		async with self.database.session_local() as session:
+
+			config = await self.database.model_config.delete_config(database = session, id = request.id)
+
+			return protocol.database_pb2.ConfigResponse(id = request.id, key = request.key, value = request.value)
+
 async def serve(database):
 
 	server = grpc.aio.server()
 	protocol.database_pb2_grpc.add_DatabaseServiceServicer_to_server(DatabaseService(database), server)
 	server.add_insecure_port('[::]:50051')
+
 	await server.start()
 	print("Server started. Listening on port 50051...")
 
 	stop_event = asyncio.Event()
 
 	def shutdown_signal(*args):
+
 		print("Shutting down server...")
 		stop_event.set()
 
