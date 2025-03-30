1f9ca06cc196b726e02f39fdde08ada64fb52dd2
tayadj <tayadj@users.noreply.github.com>
2025-03-30 21:24:58 +0300

    Implemented audio streaming web-interface

--- src/application/src/main.py
+++ src/application/src/main.py
@@ -63,6 +63,8 @@ class Application:
 
 	def __init__(self, settings, engine):
 
+		self.settings = settings
+		self.engine = engine
 		self.application = flask.Flask(__name__, template_folder = os.path.dirname(__file__) + '\\templates')
 		self.socketio = flask_socketio.SocketIO(self.application, async_mode = 'threading')
 		self.application.config['SECRET_KEY'] = settings.APPLICATION_SECRET_KEY.get_secret_value()
@@ -72,9 +74,40 @@ class Application:
 	def setup(self):
 
 		@self.application.route('/')
-		def main():
+		def index():
 
 			return flask.render_template('index.html')
+
+		@self.socketio.on('start')
+		def handle_start():
+
+			async def process():
+
+				async def stream_audio_input():
+
+					async for data in self.engine.audio_input():
+
+						yield simulator.src.protocol.simulator_pb2.AudioStream(data = data.tobytes())
+
+				async with grpc.aio.insecure_channel(self.settings.SIMULATOR_PORT.get_secret_value()) as channel: 
+	
+					stub = simulator.src.protocol.simulator_pb2_grpc.SimulatorServiceStub(channel)
+
+					async for chunk in stub.StreamAudio(stream_audio_input()):
+
+						print(f"Received chunk of size: {len(chunk.data)} bytes.")
+
+						data = numpy.frombuffer(chunk.data, dtype = numpy.int16)
+
+						await self.engine.audio_output(data)			
+
+			self.engine.start_audio_stream()
+			threading.Thread(target = asyncio.run(process())).start()
+
+		@self.socketio.on('end')
+		def handle_end():
+
+			self.engine.stop_audio_stream()
 
 	async def run(self):
 
