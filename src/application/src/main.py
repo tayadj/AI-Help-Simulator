2ab9f7146fca670318e43be30bfc5d49b3d18ee0
tayadj <tayadj@users.noreply.github.com>
2025-04-08 22:58:52 +0300

    Docker compose added

--- src/application/src/main.py
+++ src/application/src/main.py
@@ -15,49 +15,7 @@ sys.path.append(os.path.dirname(__file__) + '/../..')
 import database.src.protocol
 import simulator.src.protocol
 
-'''
 
-async def stream_audio_input(engine):
-
-	async for data in engine.audio_input():
-
-		yield simulator.src.protocol.simulator_pb2.AudioStream(data = data.tobytes())
-
-async def main(settings, engine):
-
-	async with grpc.aio.insecure_channel(settings.DATABASE_PORT.get_secret_value()) as channel: 
-	
-		stub = database.src.protocol.database_pb2_grpc.DatabaseServiceStub(channel)
-
-		request = database.src.protocol.database_pb2.UserRequest(
-			id = 1
-		)
-
-		response = await stub.ReadUser(request)  
-		print(response)
-
-	async with grpc.aio.insecure_channel(settings.SIMULATOR_PORT.get_secret_value()) as channel: 
-	
-		stub = simulator.src.protocol.simulator_pb2_grpc.SimulatorServiceStub(channel)
-
-		async for chunk in stub.StreamAudio(stream_audio_input(engine)):
-
-			print(f"Received chunk of size: {len(chunk.data)} bytes.")
-
-			data = numpy.frombuffer(chunk.data, dtype = numpy.int16)
-
-			await engine.audio_output(data)
-			
-
-
-if __name__ == "__main__":
-
-	settings = config.Settings()
-	engine = core.Engine()
-
-	asyncio.run(main(settings, engine))
-
-'''
 
 class Application:
 
